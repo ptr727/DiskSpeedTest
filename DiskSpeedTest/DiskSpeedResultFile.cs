16932a3396fd2337e0612a3fc075e1e615ace79a
Pieter Viljoen <ptr727@users.noreply.github.com>
2020-02-08 19:25:57 -0800

    Log using text labels.

--- DiskSpeedTest/DiskSpeedResultFile.cs
+++ DiskSpeedTest/DiskSpeedResultFile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using InsaneGenius.Utilities;
 
 namespace DiskSpeedTest
 {
@@ -26,7 +27,7 @@ namespace DiskSpeedTest
                 throw new ArgumentNullException(nameof(testResult));
 
             // Add a result line
-            string result = $"{DateTime.UtcNow:s}, \"{testTarget.FileName}\", {testTarget.FileSize}, {testParameter.BlockSize}" +
+            string result = $"{DateTime.UtcNow:s}, \"{testTarget.FileName}\", {testTarget.FileSize}, {Format.BytesToKibi(testParameter.BlockSize)}" +
                 $", {testParameter.WriteRatio}, {testParameter.ThreadCount}, {testParameter.OutstandingOperations}" + 
                 $", {testParameter.WarmupTime}, {testResult.Seconds}, {testResult.Bytes}, {testResult.Ios}";
             File.AppendAllText(FileName, result + Environment.NewLine);
@@ -40,7 +41,7 @@ namespace DiskSpeedTest
                 throw new ArgumentNullException(nameof(testParameter));
 
             // Add a result line
-            string result = $"{DateTime.UtcNow:s}, \"{testTarget.FileName}\", {testTarget.FileSize}, {testParameter.BlockSize}" +
+            string result = $"{DateTime.UtcNow:s}, \"{testTarget.FileName}\", {testTarget.FileSize}, {Format.BytesToKibi(testParameter.BlockSize)}" +
                 $", {testParameter.WriteRatio}, {testParameter.ThreadCount}, {testParameter.OutstandingOperations}" +
                 $", {testParameter.WarmupTime}, 0, 0, 0";
             File.AppendAllText(FileName, result + Environment.NewLine);
